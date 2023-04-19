9fa851383abb456a2a72f00dd1e74260efed0a22
vasMil <87028699+vasMil@users.noreply.github.com>
2023-04-19 23:20:32 +0300

    Removes empty_env (UE4 map)

--- empty_env/Source/empty_env/empty_env.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "empty_env.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, empty_env, "empty_env" );
